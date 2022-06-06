f9f700740a276939846d131199d0009444e7a365
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-06-06 23:49:36 +0530

    Update StockbuyandSell.cpp

--- day3/StockbuyandSell.cpp
+++ day3/StockbuyandSell.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = INT_MAX;
+        int n = prices.size();
+        int profit = 0;
+        for(auto it : prices){
+            mini = min(mini, it);
+            profit = max(profit, (it-mini));
+        }
+        return profit;
+    }
+};

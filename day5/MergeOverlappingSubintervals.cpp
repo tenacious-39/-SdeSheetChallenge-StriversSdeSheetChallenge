b9de5aa28d97ada64ec02f4649a89500cc4770aa
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-06-09 20:38:24 +0530

    Update MergeOverlappingSubintervals.cpp

--- day5/MergeOverlappingSubintervals.cpp
+++ day5/MergeOverlappingSubintervals.cpp
@@ -18,3 +18,23 @@ public:
         return ans;
     }
 };
+
+// Brute force approach:
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        int n = 1;
+        for(auto it : intervals){
+            if(it[0] <= ans[n-1][1]){
+                ans[n-1][1] = max(ans[n-1][1], it[1]);
+            } else{
+                ans.push_back(it);
+                n++;
+            }
+        }
+        return ans;
+    }
+};

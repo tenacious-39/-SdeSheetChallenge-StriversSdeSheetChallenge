920b0680c2c0ce70de862f9faaf0ced154f93e94
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-06-10 21:10:24 +0530

    Update FindtheDuplicateNumber.cpp

--- day7/FindtheDuplicateNumber.cpp
+++ day7/FindtheDuplicateNumber.cpp
@@ -1,1 +1,30 @@
 
+
+// My code 1:
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i-1]) return nums[i];
+        }
+        return -1;
+    }
+};
+
+
+// My code 2:
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int, int> mp;
+        for(auto it : nums){
+            if(mp[it] == 1){
+                return it;
+            }
+            mp[it]++;
+        }
+        return -1;
+    }
+};

908e8d7a7ab9630964e57f4db0e7ca38d7b2b189
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-06-16 21:53:09 +0530

    Create MiddleoftheLinkedList.cpp

---
+++ day13/MiddleoftheLinkedList.cpp
@@ -0,0 +1,24 @@
+// Slow and fast tortoise:
+// sde sheet solution and
+//My solution:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while(fast && fast->next){
+            fast = (fast->next)->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};

37b449ac8c8f0a62e2a24ad3a5f78cfae951d30d
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-07-08 17:58:18 +0530

    Update subsetSumII.cpp

--- day35/subsetSumII.cpp
+++ day35/subsetSumII.cpp
@@ -1,3 +1,26 @@
+//// Optimised approach:
+class Solution {
+    void getSubset(vector<int> &nums, int n, int idx, vector<int> &ds, vector<vector<int>> &ans){
+        ans.push_back(ds);
+        for(int i = idx; i < n; i++){
+            if(i != idx && nums[i] == nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            getSubset(nums, n, i+1, ds, ans);
+            ds.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        getSubset(nums, n, 0, ds, ans);
+        return ans;
+    }
+};
+
+
 
 /// My approach:
 class Solution {

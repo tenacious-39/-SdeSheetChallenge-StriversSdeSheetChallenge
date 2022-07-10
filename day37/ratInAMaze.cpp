5eb7713d55c9ef709aff13a027c0c0cabbf04207
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-07-10 15:46:56 +0530

    Update ratInAMaze.cpp

--- day37/ratInAMaze.cpp
+++ day37/ratInAMaze.cpp
@@ -1,3 +1,32 @@
+//---------------------------------------------------------------------------------------------------------------------------------------------
+//        ##More clean Code
+
+void solve(int row, int col, vector<vector<int>> &m, int n, string move, vector<string> &ans, vector<vector<int>> &vis, int di[], int dj[]){
+        if(row == n-1 and col == n-1){
+            ans.push_back(move);
+            return;
+        }
+        string s = "DLRU";
+        for(int i = 0; i < 4; i++){
+            if(row+di[i] >= 0 and row+di[i] < n and col+dj[i] >= 0 and col+dj[i] < n and !vis[row+di[i]][col+dj[i]] and m[row+di[i]][col+dj[i]] == 1){
+                vis[row][col] = 1;
+                solve(row+di[i], col+dj[i], m, n, move+s[i], ans, vis, di, dj);
+                vis[row][col] = 0;
+            }
+        }
+    }
+    public:
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+        vector<string> ans;
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        int di[] = {1, 0, 0, -1};
+        int dj[] = {0, -1, 1, 0};
+        if(m[0][0] == 1) solve(0, 0, m, n, "", ans, vis, di, dj);
+        return ans;
+    }
+
+//-------------------------------------------------------------------------------------------------------------------------------
 void solve(int row, int col, vector<vector<int>> &m, int n, string move, vector<string> &ans, vector<vector<int>> &vis){
         if(row == n-1 and col == n-1){
             ans.push_back(move);

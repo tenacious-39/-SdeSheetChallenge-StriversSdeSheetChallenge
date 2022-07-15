fc99e11764801db9e9c126428cfc091663353265
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-07-15 20:38:43 +0530

    Create implementStackUsingQueue.cpp

---
+++ day42/implementStackUsingQueue.cpp
@@ -0,0 +1,43 @@
+class MyStack {
+    queue<int> q;
+    int cnt;
+public:
+    MyStack() {
+        cnt = 0;
+    }
+    
+    void push(int x) {
+        queue<int> tmp;
+        tmp.push(x);
+        while(!q.empty()){
+            tmp.push(q.front());
+            q.pop();
+        }
+        q = tmp;
+        cnt++;
+    }
+    
+    int pop() {
+        int ans = q.front();
+        q.pop();
+        cnt--;
+        return ans;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return cnt == 0;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */

f662e3a647d470eb73496311bca2e88af935e654
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-07-22 22:24:26 +0530

    Update topologicalSort.cpp

--- day49/topologicalSort.cpp
+++ day49/topologicalSort.cpp
@@ -30,3 +30,35 @@ class Solution
 	    return ans;
 	}
 };
+
+/// BFS traversal:
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int> indegree(V+1, 0);
+	    for(int i = 0; i < V; i++){
+	        for(auto it : adj[i]) indegree[it]++;
+	    }
+	    queue<int> q;
+	    for(int i = 0; i < V; i++){
+	        if(!indegree[i]) q.push(i);
+	    }
+	    
+	    vector<int> ans;
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        
+	        for(auto it : adj[node]){
+	            indegree[it]--;
+	            if(!indegree[it]) q.push(it);
+	        }
+	    }
+	    return ans;
+	}
+};

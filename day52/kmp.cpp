2f09858522214e8a26db2c2d0f37a439dc25859d
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-07-25 17:45:55 +0530

    Create kmp.cpp

---
+++ day52/kmp.cpp
@@ -0,0 +1,16 @@
+ vector<int> kmpProcess(string needle) {
+        int n = needle.size();
+        vector<int> lps(n, 0);
+        for (int i = 1, len = 0; i < n;) {
+            if (needle[i] == needle[len]) {
+                lps[i++] = ++len;
+            } else if (len) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
+            }
+        }
+        return lps;
+    }
+
+//ref: leetcode

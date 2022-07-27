0cf1ff7040558cc203e4929fada9165945229163
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-07-27 21:55:14 +0530

    Create searchKeyInBST.cpp

---
+++ day54/searchKeyInBST.cpp
@@ -0,0 +1,25 @@
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    // Write your code here.
+    while(root != NULL){
+        if(root->data == x) return true;
+        root = x < root->data? root->left : root->right;
+    }
+    return false;
+}

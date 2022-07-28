7e49fc2a0b09cfbfcde6e9243eac2c0ca7a0d592
Kritidipta Ghosh <86186712+striver-80@users.noreply.github.com>
2022-07-28 19:23:05 +0530

    Create Maximum XOR of Two Numbers in an Array.cpp

---
+++ Maximum XOR of Two Numbers in an Array.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+struct Node {
+  Node * links[2];
+ 
+  bool containsKey(int ind) {
+    return (links[ind] != NULL);
+  }
+  Node * get(int ind) {
+    return links[ind];
+  }
+  void put(int ind, Node * node) {
+    links[ind] = node;
+  }
+};
+class Trie {
+  private: Node * root;
+  public:
+    Trie() {
+      root = new Node();
+    }
+ 
+  public:
+    void insert(int num) {
+      Node * node = root;
+      // cout << num << endl; 
+      for (int i = 31; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        if (!node -> containsKey(bit)) {
+          node -> put(bit, new Node());
+        }
+        node = node -> get(bit);
+      }
+    }
+  public:
+    int findMax(int num) {
+      Node * node = root;
+      int maxNum = 0;
+      for (int i = 31; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        if (node -> containsKey(!bit)) {
+          maxNum = maxNum | (1 << i);
+          node = node -> get(!bit);
+        } else {
+          node = node -> get(bit);
+        }
+      }
+      return maxNum;
+    }
+};
+int maximumXor(vector<int> A)
+{
+    // Write your code here.  
+    Trie trie;
+    int n= A.size();
+      for (int i = 0; i < n; i++) {
+        trie.insert(A[i]);
+      }
+      int maxi = 0;
+      for (int i = 0; i < n; i++) {
+        maxi = max(maxi, trie.findMax(A[i]));
+      }
+      return maxi;
+}
